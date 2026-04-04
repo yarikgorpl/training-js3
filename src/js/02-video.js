abe7adb9eb5c3bb6b1fa7960491b5fbc2ee21087
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2026-04-04 22:00:46 +0300

    Finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime)).catch(error => {
+    console.error('Помилка відновлення часу:', error);
+  });
+}
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
